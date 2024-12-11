8a10652ff6f9892f9ac56d385b04fa8027b60de2
TulqinWeb <tulqinurinov005@gmail.com>
2024-12-11 10:07:33 +0500

    added table to fastfood_db.py

--- fastfood_db.py
+++ fastfood_db.py
@@ -11,3 +11,53 @@ class Database:
             db_port = DB_PORT
         )
         self.cursor = self.conn.cursor()
+
+        self.cursor.execute("""
+            CREATE TABLE IF NOT EXISTS users(
+                id SERIAL PRIMARY KEY,                 
+                telegram_id BIGINT NOT NULL,            
+                username VARCHAR(50),                  
+                full_name VARCHAR(50),                
+                phone_number VARCHAR(20),               
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            """)
+
+        self.cursor.execute("""
+            CREATE TABLE IF NOT EXISTS products (
+                id SERIAL PRIMARY KEY,
+                name VARCHAR(255) NOT NULL,    
+                price DECIMAL(10, 2) NOT NULL, 
+                image_url TEXT                 
+                );
+             """)
+
+        self.cursor.execute("""
+            CREATE TABLE IF NOT EXISTS orders (
+                id SERIAL PRIMARY KEY,
+                user_id INT REFERENCES users(id),  
+                total_price DECIMAL(10, 2),        
+                status VARCHAR(50) DEFAULT 'pending',
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                );
+             """)
+
+        self.cursor.execute("""
+            CREATE TABLE order_items (
+                id SERIAL PRIMARY KEY,
+                order_id INT REFERENCES orders(id),  
+                product_id INT REFERENCES products(id), 
+                quantity INT NOT NULL                
+                );
+             """)
+
+        self.cursor.execute("""
+            CREATE TABLE IF NOT EXISTS categories (
+                id SERIAL PRIMARY KEY,        
+                name VARCHAR(50) NOT NULL,     
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                );
+            """)
+
+
+
+
