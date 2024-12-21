00d5667af2f9d75579f389094a365679008da072
TulqinWeb <tulqinurinov005@gmail.com>
2024-12-21 23:54:13 +0500

    added decorator to check information but not fully

--- message_handler.py
+++ message_handler.py
@@ -1,45 +1,39 @@
-# from register import check, check_data_decorator
+import globals
+from decorator import main_menu
 from fastfood_db import Database
-# import globals
 
 db = Database()
 
 
-# @check_data_decorator
 async def message_handler(update, context):
-    pass
-    # message = update.message.text
-    # user = update.message.from_user
-    # state = context.user_data.get('state', 0)
-    # db_user = db.get_user_by_chat_id(user.id)
-    # if state == 0:
-    #    print(state)
-    #    await check(update, context)
-    #
-    # elif state == 1:
-    #     if not db_user['lang_id']:
-    #         if message == globals.BTN_LANG_UZ:
-    #             db.update_user_data(user.id, 'lang_id', 1)
-    #             await check(update, context)
-    #
-    #         elif message == globals.BTN_LANG_RU:
-    #             db.update_user_data(user.id, 'lang_id', 2)
-    #             await check(update, context)
-    #
-    #         else:
-    #             await update.message.reply_text(text=globals.TEXT_LANG_WARNING)
-    #
-    #     elif not db_user['first_name']:
-    #         db.update_user_data(user.id, 'first_name', message)
-    #         await check(update, context)
-    #
-    #     elif not db_user['last_name']:
-    #         db.update_user_data(user.id, 'last_name', message)
-    #         await check(update, context)
-    #
-    #     elif not db_user['phone_number']:
-    #         db.update_user_data(user.id, 'phone_number', message)
-    #         await check(update, context)
-    #
-    #     else:
-    #         pass
+    missing_field = context.user_data.get("missing_field")
+    user = update.message.from_user
+
+    if missing_field == "first_name":
+        first_name = update.message.text
+        db.update_user_data(user.id, "first_name", first_name)
+        context.user_data.pop("missing_field")
+        return await main_menu(update, context)
+
+    if missing_field == "last_name":
+        last_name = update.message.text
+        db.update_user_data(user.id, "last_name", last_name)
+        context.user_data.pop("missing_field")
+        return await main_menu(update, context)
+
+    if missing_field == "phone_number":
+        db_user = db.get_user_by_chat_id(update.message.from_user.id)
+        lang_id = db_user["lang_id"]
+        contact = update.message.contact
+        if contact and contact.phone_number:
+            phone_number = contact.phone_number
+            db.update_user_data(user.id, "phone_number", phone_number)
+            context.user_data.pop("missing_field")
+            return await main_menu(update, context)
+
+        else:
+            await update.message.reply_text(
+                text=globals.TEXT_ENTER_CONTACT_WARNING[lang_id]
+            )
+            return
+
